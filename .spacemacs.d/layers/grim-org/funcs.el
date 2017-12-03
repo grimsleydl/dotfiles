11fad6ea7b3b72d6d441ae618e9c90510c627e4a
david grimsley <grimsleydl@gmail.com>
2017-12-02 22:19:45 -0600

    tmux tweak, org helpers

--- .spacemacs.d/layers/grim-org/funcs.el
+++ .spacemacs.d/layers/grim-org/funcs.el
@@ -1,3 +1,31 @@
+(defun my-smarter-kill-ring-save ()
+  (interactive)
+  (if (region-active-p)
+      (call-interactively #'kill-ring-save)
+    (when (eq major-mode 'org-mode)
+      (call-interactively #'my-org-retrieve-url-from-point))))
+
+(defun my-yank-org-link (text)
+  (if (derived-mode-p 'org-mode)
+      (insert text)
+    (string-match org-bracket-link-regexp text)
+    (insert (substring text (match-beginning 1) (match-end 1)))))
+
+(defun my-org-retrieve-url-from-point ()
+  (interactive)
+  (let* ((link-info (assoc :link (org-context)))
+         (text (when link-info
+                 ;; org-context seems to return nil if the current element
+                 ;; starts at buffer-start or ends at buffer-end
+                 (buffer-substring-no-properties (or (cadr link-info) (point-min))
+                                                 (or (caddr link-info) (point-max))))))
+    (if (not text)
+        (error "Not in org link")
+      (add-text-properties 0 (length text) '(yank-handler (my-yank-org-link)) text)
+      (kill-new text))))
+
+
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; function to wrap blocks of text in org templates                       ;;
 ;; e.g. latex or src etc                                                  ;;
