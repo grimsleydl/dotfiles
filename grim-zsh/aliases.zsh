3456665ba56b19809fd93629e4dccb2949d5f587
grimsleydl <grimsleydl@gmail.com>
2018-02-08 03:36:31 -0600

    zsh config updates

--- grim-zsh/aliases.zsh
+++ grim-zsh/aliases.zsh
@@ -1,31 +1,15 @@
 alias _='sudo'
-alias xx='patool extract'     #size,show type,human readable
-alias killit='kill -9 %%'
+alias als='alias | grep -i --'
 
 alias dfh='df -h'
-
-alias als='alias | grep -i --'
-alias logs="journalctl -f -u"
-
-# zypper
-alias zyp='zypper'
-unalias zse
-zse() {
-    zypper se -s "$1" | grep -v "| i586   |" # search, filter x86 pacages
-}
-
 alias Gi="| grep -i"
-alias psa="ps aux"
-alias psg="ps aux | grep"
-
-alias Y="| yank"
 
 alias ka9='killall -9'
 alias k9='kill -9'
+alias killit='kill -9 %%'
 
-alias scu='systemctl --user'
+alias logs="journalctl -f -u"
 
-alias rival='sudo rival'
 # if exa is installed
 if (( $+commands[exa] )) ; then
     # alias ls=exa
@@ -33,8 +17,26 @@ if (( $+commands[exa] )) ; then
     alias la='exa -la'
     alias ll='exa -alF'
     alias lh='exa -alh'
-
 fi
+
+alias psa="ps aux"
+alias psc='ps xawf -eo pid,user,cgroup,args'
+alias psg="ps aux | grep"
+alias rival='sudo rival'
+alias scu='systemctl --user'
+alias xx='patool extract'     #size,show type,human readable
+
+alias Y="| yank"
+alias yaa="yadm add"
+alias yac="yadm commit"
+alias ya="yadm commit -a -m"
+
+# zypper
+alias zyp='zypper'
+unalias zse
+zse() {
+    zypper se -s "$1" | grep -v "| i586   |" # search, filter x86 pacages
+}
 
 # get keyboard devnode
 alias getkeyboard="grep -E  'Handlers|EV=' /proc/bus/input/devices | grep -B1 'EV=120013' | grep -Eo 'event[0-9]+'"
