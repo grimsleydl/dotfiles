79f798935bbf874cb57da9dc2e3c7eed713dd5e3
dlgrimsley <dlgrimsley@gmail.com>
2018-02-13 07:57:32 -0600

    new zsh functions

--- grim-zsh/functions.zsh
+++ grim-zsh/functions.zsh
@@ -5,3 +5,53 @@ zdiff() {
 yacp() {
     yadm add . && yadm commit -m "$1" && yadm push
 }
+
+############################
+# https://p.teknik.io/2349 #
+############################
+dot_progress() {
+    # Fancy progress function from Landley's Aboriginal Linux.
+    # Useful for long rm, tar and such.
+    # Usage:
+    #     rm -rfv /foo | dot_progress
+    local i='0'
+    local line=''z
+
+    while read line; do
+        i="$((i+1))"
+        if [ "${i}" = '25' ]; then
+            printf '.'
+            i='0'
+        fi
+    done
+    printf '\n'
+}
+
+rmssay()
+{
+    length=${#1}
+
+    printf "┌─"; for ((c=1; c<=$length; c++)); do printf "─"; done; printf "─┐\n"
+    printf "│ $1 │\n"
+    printf "└─"; for ((c=1; c<=$length; c++)); do printf "─"; done; printf "─┘\n"
+    printf "      \\ \n"
+    printf "       \\ \n"
+    printf "        \\ \n"
+    printf "          @@@@@@ @
+        @@@@     @@
+       @@@@ =   =  @@
+      @@@ @ _   _   @@
+      @@@ @($)|($)  @@
+     @@@@   ~ | ~   @@
+     @@@ @  (o1o)    @@
+    @@@    #######    @
+    @@@   ##{+++}##   @@
+   @@@@@ ## ##### ## @@@@
+   @@@@@#############@@@@
+  @@@@@@@###########@@@@@@
+ @@@@@@@#############@@@@@
+ @@@@@@@### ## ### ###@@@@
+  @ @  @              @  @
+    @                    @
+"
+}
